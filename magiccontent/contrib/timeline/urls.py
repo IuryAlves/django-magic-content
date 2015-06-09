3fcb5813b4c128be25ce8b0dae2928fdfab56fd9
Heigler Rocha <heigler.rocha@djenie.com>
2015-06-09 10:36:04 -0300

    add showpage for timeline

--- magiccontent/contrib/timeline/urls.py
+++ magiccontent/contrib/timeline/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls import url, patterns
 
 from magiccontent.dynamic_content_urls import get_content_urls_for
 from .models import EntryContent
-from .views import EntryDetailView
+from .views import EntryDetailView, ShowEntryContentPageView
 
 urlpatterns = patterns("",  # noqa
+    url(r'^(?P<entry_type>[-\w]+)/(?P<pk>\d+)/$',
+        ShowEntryContentPageView.as_view(),
+        name='timeline.entrycontent.showpage'),
     url(r'^(?P<entry_type>[-\w]+)/(?P<entry_slug>[-\w]+)/(?P<pk>\d+)/$',
         EntryDetailView.as_view(), name='timeline.entrycontent.detail'),
 )
--- magiccontent/contrib/timeline/views.py
+++ magiccontent/contrib/timeline/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
 
+from accounts.mixins import CanEditMixin
+
+from magiccontent.models import Widget
 from magiccontent.mixins import (EditableMixin, CreateContentMixin,
                                  ListContentMixin)
 from .models import EntryContent
@@ -40,3 +45,17 @@ class EntryDetailView(DetailView):
     def get_queryset(self):
         return self.model.objects.filter(
             entry_type=self.kwargs.get('entry_type'))
+
+
+class ShowEntryContentPageView(CanEditMixin, TemplateView):
+    template_name = "magiccontent/timeline.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowEntryContentPageView,
+                        self).get_context_data(**kwargs)
+        widget = get_object_or_404(Widget, pk=self.kwargs.get('pk', None))
+        context['widget'] = widget
+        context['content_list'] = widget.get_widget_type.objects.filter(
+            widget=widget)
+        raise Exception(context)
+        return context
